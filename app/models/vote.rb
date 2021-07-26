a15b12a50890865d9980487642a117b223e6a4a4
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-26 03:30:49 +0100

    Add styles to articles show and add vote count for articles

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,5 +4,5 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :article
 
-  
+  scope :popular, -> { select('article_id, count(article_id) as count').group(:article_id).order('count desc').limit(1) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
   resources :articles do
     resources :votes, only: [:create, :destroy]
   end
   resources :categories
   devise_for :users
-  root 'articles#index'
+  root 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
