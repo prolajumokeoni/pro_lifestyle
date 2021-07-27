495bfb21df3fc98ea0ce32b1078f859b07d03fa5
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-27 15:36:58 +0100

    Add negative tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { should belong_to(:user) }
   it { should have_many(:articles) }
+  
+  
+  it 'is not valid with nil values' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 end
