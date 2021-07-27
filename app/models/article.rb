22f5274ed1dce40564d0e3da50d84e456b92ffa3
prolajumokeoni <prolajumokeoni@gmail.com>
2021-07-27 13:16:23 +0100

    Add validations to forms

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,9 +3,4 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :votes, dependent: :destroy
   has_one_attached :article_photo
-  after_create :self_vote
-
-  def self_vote
-    votes.create(user: user)
-  end
 end
